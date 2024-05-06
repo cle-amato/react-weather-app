4e5d758b05cb04b39d76869d579dd969cc8557ed
cle-amato <cle.amato@gmail.com>
2024-05-06 16:02:22 +0100

    imported bootstrap

--- src/App.js
+++ src/App.js
@@ -6,19 +6,21 @@ export default function App() {
   return (
     <div>
       <div className="App">
-        <Search />
-        <Weather />
+        <div className="container">
+          <Search />
+          <Weather />
+        </div>
+        <footer>
+          <a
+            href="https://github.com/cle-amato/react-weather-app"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Open source code
+          </a>
+          , by Clementina Amato
+        </footer>
       </div>
-      <footer>
-        <a
-          href="https://github.com/cle-amato/react-weather-app"
-          rel="noreferrer"
-          target="_blank"
-        >
-          Open source code
-        </a>
-        , by Clementina Amato
-      </footer>
     </div>
   );
 }
