fa37aa6e11f15cc88266aa6a4b1aea99a1b78b3f
cle-amato <cle.amato@gmail.com>
2024-05-18 15:04:02 +0100

    added Forecast for 5 days

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,9 +17,15 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          <div className="col">
-            <WeatherForecastDay data={forecastData[2]} />
-          </div>
+          {forecastData.map(function (dailyForecast, index) {
+            if (index < 6) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     );
